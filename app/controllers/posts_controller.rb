c531d3bbdf70af53e603260b649b6a02fb70aaf1
nutsmakadamia <nutsmakadamia@gmail.com>
2018-06-29 02:13:07 +0900

    migrate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,15 @@ class PostsController < ApplicationController
   end
 
   # POST /posts
-  def create
-    render plain: params[:post].inspect
+def create
+  @post = Post.new(post_params)
+  @post.save
+  redirect_to @post
+end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
+  
 end
